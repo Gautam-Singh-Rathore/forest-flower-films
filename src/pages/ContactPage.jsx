5d2d3551c79f4ae23a1f0bf92433ff0ae2601154
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2025-06-20 14:26:21 +0530

    Changes

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,8 +5,8 @@ const ContactPage = () => {
   return (
     <div className="w-full h-full text-white bg-black min-h-screen">
       <div className="relative bg-cover bg-center w-full h-full lg:px-[10vw] md:px-[10vw] ">
-        <img src={contact} className="h-[30vh] md:h-auto object-cover" />
-        <h1 className="absolute top-[35%] text-black text-[60px] font-extrabold px-10 hidden md:flex">
+        {/* <img src={contact} className="h-[30vh] md:h-auto object-cover" /> */}
+        <h1 className=" text-white text-[60px] font-extrabold px-10 hidden md:flex">
           Contact Us{" "}
         </h1>
         </div>
@@ -22,6 +22,7 @@ const ContactPage = () => {
               <div>
                 <p className="font-bold md:text-xl">Ajender Singh</p>
                 <p className="text-sm md:text-lg">ajender@forestflowerfilms.com</p>
+                <p className="text-sm md:text-lg">7895817157</p>
               </div>
             </div>
           </div>
